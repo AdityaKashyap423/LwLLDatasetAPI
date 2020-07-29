6ddf8f13182cc9868ed4dd5e8796d9f9394cb9d0
Aditya M. Kashyap <kashyap@seas.upenn.edu>
2020-07-29 17:58:23 -0400

    Added Code and README

---
+++ LwLLDataAPI.py
@@ -0,0 +1,342 @@
+import requests
+import os
+import json
+import pandas as pd
+from pathlib import Path
+from typing import Tuple, List
+import random
+import numpy as np
+import argparse
+
+
+url = 'https://api-dev.lollllz.com'
+data_type = 'full'
+task_id = '06023f86-a66b-4b2c-8b8b-951f5edd0f22' # For machine translation
+
+
+def get_command_line_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-secret','--secret',required=True)
+    parser.add_argument('-mode','--mode', required=True)
+    parser.add_argument('-data_folder','--data_folder')
+    parser.add_argument('-pred_path','--pred_path')
+    parser.add_argument('-save_path','--save_path')
+    args = vars(parser.parse_args())
+    global secret 
+    secret = args["secret"]
+    mode = args["mode"]
+    data_path = args["data_folder"]
+    pred_path = args["pred_path"]
+    save_path = args["save_path"]
+
+    if (mode == "training_data_new" or mode == "training_data_continue") and (data_path is None or save_path is None):
+    	print("For mode = training_data_new/continue, --data_folder and --save_path is required!")
+    	exit(1)
+
+    if mode == "submit_predictions" and (pred_path is None or save_path is None):
+    	print("For mode = submit_predictions, --pred_path and --save_path is required!")
+    	exit(1)
+
+    return mode,data_path,pred_path,secret,save_path
+
+
+def create_session():
+
+	headers = {'user_secret': secret}
+
+	r = requests.post(f"{url}/auth/create_session", json={'session_name': 'testing', 'data_type': data_type, 'task_id': task_id}, headers=headers)
+	session_token = r.json()['session_token']
+
+	print("Created session with token: ",session_token)
+
+	return session_token
+
+  
+
+def deactivate_session(session_token,session="all"):
+	
+	headers_session = {'user_secret': secret}
+	r = requests.get(f"{url}/list_active_sessions", headers=headers_session)
+	active_sessions = r.json()
+	all_active_sessions = active_sessions['active_sessions']
+
+	headers_active_session = {'user_secret': secret, 'session_token': session_token}
+
+	if session == "all":
+		for current_session in set(all_active_sessions) - set([session_token]): # Deactivate all sessions apart from the current session
+			print("Deactivation Session: " + current_session)
+			r = requests.post(f"{url}/deactivate_session", json={'session_token': current_session}, headers=headers_active_session)
+	else:
+		if session not in all_sessions:
+			print("ERROR: This session does not exist!")
+		else:
+			r = requests.post(f"{url}/deactivate_session", json={'session_token': session}, headers=headers_active_session)
+
+
+def get_train_data_mt(dataset_path: Path, session_token: str) -> List[str]:
+    """
+    Helper method to dynamically get the test labels and give us the possible classes that can be submitted
+    for the current dataset
+    
+    Params
+    ------
+    
+    dataset_path : Path
+        The path to the `development` dataset downloads
+    
+    session_token : str
+        Your current session token so that we can look up the current session metadata
+    
+    Returns
+    -------
+    
+    pd.DataFrame
+        The DataFrame on which you can make queries against
+    """
+    # Then we can just reference our current metadata to get our dataset name and use that in the path
+    headers = {'user_secret': secret, 'session_token': session_token}
+    r = requests.get(f"{url}/session_status", headers=headers)
+    current_dataset = r.json()['Session_Status']['current_dataset']
+    current_dataset_name = current_dataset['name']
+
+
+    test_df = pd.read_feather(str(dataset_path.joinpath(f"{current_dataset_name}/{current_dataset_name}_{data_type}/train_data.feather")))
+    return test_df
+
+def get_test_data_mt(dataset_path: Path, session_token: str) -> List[str]:
+    """
+    Helper method to dynamically get the test labels and give us the possible classes that can be submitted
+    for the current dataset
+    
+    Params
+    ------
+    
+    dataset_path : Path
+        The path to the `development` dataset downloads
+    
+    session_token : str
+        Your current session token so that we can look up the current session metadata
+    
+    Returns
+    -------
+    
+    pd.DataFrame
+        The DataFrame on which you must make predictions from a 'source' column
+    """
+    # Then we can just reference our current metadata to get our dataset name and use that in the path
+    headers = {'user_secret': secret, 'session_token': session_token}
+    r = requests.get(f"{url}/session_status", headers=headers)
+    current_dataset = r.json()['Session_Status']['current_dataset']
+    current_dataset_name = current_dataset['name']
+    
+    _path = str(dataset_path.joinpath(f"{current_dataset_name}/{current_dataset_name}_{data_type}/test_data.feather"))
+    test_df = pd.read_feather(_path)
+    return test_df
+
+
+def get_training_labels(all_training_ids,already_queried,train_df,session_token):
+
+	retrieval_cap = 25000 # set by the LwLL server
+
+	train_df = df_to_dict(train_df)
+
+	headers = {'user_secret': secret, 'session_token': session_token}
+	r = requests.get(f"{url}/session_status", headers=headers).json()
+
+	required_ids = list(set(all_training_ids) - set(already_queried))
+
+	num_parts = int(len(required_ids)/retrieval_cap) + 1
+
+	training_labels = []
+
+	repeat = True
+
+
+	while repeat:
+
+		for i in range(num_parts):
+			required_ids_part = required_ids[i*retrieval_cap:(i+1)*retrieval_cap]
+			query = {'example_ids': required_ids_part}
+
+			r = requests.post(f"{url}/query_labels", json=query, headers=headers).json()
+
+			for instance in r['Labels']:
+				ID = instance['id']
+				eng = instance['text']
+				ar = train_df[instance['id']]
+				current_dict = {}
+				current_dict['id'] = ID 
+				current_dict['english'] = eng
+				current_dict['arabic'] = ar 
+				training_labels.append(current_dict)
+				already_queried.append(ID)
+
+			budget_left = r['Session_Status']['budget_left_until_checkpoint']
+
+			if budget_left == 0:
+				repeat = False
+				break
+
+		if budget_left > 0:
+			already_queried = []
+			required_ids = all_training_ids[:]
+			num_parts = int(len(required_ids)/retrieval_cap) + 1
+
+
+	return training_labels,already_queried
+
+
+def df_to_dict(df):
+	final_dict = {}
+
+	df = df.to_dict('records')
+	for instance in df:
+		final_dict[instance['id']] = instance['source']
+	return final_dict
+
+
+def save_data(data,already_queried,session_token,checkpoint_number,data_type,save_path):
+
+	all_data = {}
+
+	if checkpoint_number == 8:
+		already_queried = [] # Because we start with a different dataset at checkpoint 9
+
+	metadata = {}
+	metadata['already_queried'] = already_queried
+	metadata['session_token'] = session_token
+	metadata['checkpoint_number'] = checkpoint_number
+
+
+	all_data["train"] = data
+	all_data["metadata"] = metadata
+
+
+	if data_type == "train":
+		np.save(save_path + "train.npy",all_data)
+	elif data_type == "test":
+		np.save(save_path + "test.npy",data)
+
+	
+def load_previous_checkpoint_data(save_path):
+	save_path = save_path + "train.npy"
+	data = np.load(save_path,allow_pickle=True).item()
+	return data['metadata']
+
+
+
+def training_data_new(path,save_path):
+
+	checkpoint_number = 1
+
+	session_token = create_session()
+	deactivate_session(session_token) # Deactivates all previous sessions
+
+	DATASETS_PATH = Path(path)
+	train_df = get_train_data_mt(DATASETS_PATH, session_token) # 63947 training instances: id, source
+	all_training_ids = list(train_df["id"])
+
+	already_queried = []
+
+	training_data, already_queried = get_training_labels(all_training_ids,already_queried,train_df,session_token)
+
+	save_data(training_data,already_queried,session_token,checkpoint_number,"train",save_path)
+	get_test_data(session_token,DATASETS_PATH,save_path)
+
+
+def training_data_continue(path,save_path):
+
+	metadata = load_previous_checkpoint_data(save_path)
+
+	session_token = metadata['session_token']
+	already_queried = metadata['already_queried']
+	checkpoint_number = metadata['checkpoint_number'] + 1
+
+	DATASETS_PATH = Path(path)
+	train_df = get_train_data_mt(DATASETS_PATH, session_token) # 63947 training instances: id, source
+	all_training_ids = list(train_df["id"])
+
+	training_data, already_queried = get_training_labels(all_training_ids,already_queried,train_df,session_token)
+	save_data(training_data,already_queried,session_token,checkpoint_number,"train",save_path)
+	get_test_data(session_token,DATASETS_PATH,save_path)
+ 
+
+def get_test_data(session_token,path,save_path):
+
+	test_df = get_test_data_mt(path, session_token)
+	test_df = df_to_dict(test_df)
+	save_data(test_df,[],session_token,"N/A","test",save_path)
+
+
+def submit_predictions(pred_path, save_path):
+
+	metadata = load_previous_checkpoint_data(save_path)
+
+	session_token = metadata['session_token']
+	checkpoint_number = metadata['checkpoint_number']
+
+	pred_df = pd.read_csv(pred_path)
+
+	headers = {'user_secret': secret, 'session_token': session_token}
+	r = requests.post(f"{url}/submit_predictions", json={'predictions': pred_df.to_dict()}, headers=headers)
+
+
+	with open(save_path + "Submission_Response.txt","w") as f:
+		f.write(str(r.json()))
+
+
+
+
+
+def create_sample_pred_file():
+	pred_df = pd.DataFrame([{"id":a, "text":"This is random"} for a in range(63949,65949)])
+	pred_df.to_csv("/nlp/data/kashyap/LWLL/Pipeline/saved_data/sample_pred.csv",index=False)
+
+def load_file_example(save_path,data_type):
+	if data_type == "train":
+		data = np.load(save_path + "train.npy",allow_pickle=True).item()
+		data = data["train"]
+
+	elif data_type == "test":
+		data = np.load(save_path + "test.npy",allow_pickle=True).item()
+
+	print(data)
+
+	return data
+
+
+
+if __name__ == '__main__':
+
+
+	mode,data_folder,pred_path,secret,save_path = get_command_line_arguments()
+
+	# load_file_example(save_path,"test")
+
+	if mode == 'training_data_new':
+		training_data_new(data_folder, save_path)
+
+	elif mode == 'training_data_continue':
+		training_data_continue(data_folder, save_path)
+
+	elif mode == 'submit_predictions':
+		submit_predictions(pred_path, save_path)
+
+	# for i in range(16):
+	# 	if i == 0:
+	# 		training_data_new(data_folder, save_path)
+	# 	else:
+	# 		training_data_continue(data_folder, save_path)
+	# 	submit_predictions(pred_path,save_path)
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # LwLLDatasetAPI
+
+
+The script *LwLLDataAPI.py* takes the following command line arguments:
+* *--secret* The secret key
+* *--mode*
+	* *training_data_new* Creates a new session and saves the training and test data for the first checkpoint to the given folder. All previous sessions are deactivated
+	* *training_data_continue*  Continues with the most recent session and saves the training and test data for the next checkpoint
+	* *submit_predictions* Submits the given predictions to the LwLL website
+* *--data_folder* The folder in which the *ted_talks* and *global_voices* lwll datasets have been downloaded. The download instructions are provided below
+* *--pred_path* The path of the file containing the predictions on the test data. This should be a csv file with columns "id" and "text". (See the *create_sample_pred_file()* function to get a better understanding of the format)
+* *--save_path* The folder in which:
+	* the formatted training data and test data will be saved
+	* the prediction submission response will be saved
+
+## Step 1: Download the LWLL datasets
+
+The first thing that needs to be done is to download the LWLL datasets from lollllz.com/wiki/display/WA/LwLL+Infrastructure+Resources using the commands:
+
+	python download.py download_data --dataset global_voices --stage development --output DATA_FOLDER --overwrite True
+	python download.py download_data --dataset ted_talks --stage development --output DATA_FOLDER --overwrite True
+
+
+## Step 2: Create a new session and save formatted training and test files
+
+Run the following command:
+	
+	python LwLLDataAPI.py --secret SECRET_KEY --mode training_data_new --data_folder DATA_FOLDER --save_path SAVE_PATH
+
+This should create two files *train.npy* and *test.npy* in the SAVE_PATH folder
+	* *train.npy*: This is a dictionary object saved using numpy, that also contains metadata. Each training instance contains an id, the english sentence and the arabic translation
+	* *test.npy*: This is a dictionary object saved using numpy. Each test instance contains an id along with an arabic sentence
+
+Look at the function *load_file_example()* to get a better understanding of these files
+
+## Step 3: Create Prediction File and Submit it
+
+First, create the prediction file according to the required format (see the *create_sample_pred_file()* function to get a better understanding)
+
+Next, submit the prediction file using the following command:
+	
+	python LwLLDataAPI.py --secret SECRET_KEY --mode submit_predictions --pred_path PRED_FILE_PATH --save_path SAVE_PATH
+
+This will create a file *Submission_Response.txt* in the SAVE_PATH folder that records the response from the LWLL server. This completes the requirement for the first checkpoint.
+
+## Step 4: Obtain new training data and test data for the next checkpoint
+
+Run the following command:
+	
+	python LwLLDataAPI.py --secret SECRET_KEY --mode training_data_continue --data_folder DATA_FOLDER --save_path SAVE_PATH
+
+Similar to step 2, it will save a new *train.npy* file and a *test.npy* file to the SAVE_PATH folder.
+
+Following this, repeat Step 3.
+
+**NOTE:** The training data changes every checkpoint. However, the test data for the first 8 checkpoints is the same, and changes for the following 8 checkpoints. There are a total of 16 checkpoints. Once the prediction submissions for all 16 checkpoints are completed, the BLEU scores for all checkpoints will be sent back by the server. (saved in SAVE_PATH/Submission_Response.txt)
+
+## Step 5: Continue receiving training and test data, and submitting predicitons
+
+Repeat step 4 14 more times for a total of 16 checkpoints. Look at lines 325 to 330 of *LwLLDataAPI.py* for a rough sketch of the pipeline
+	
+
+
+
+
+
+
+
+
+
+
+
